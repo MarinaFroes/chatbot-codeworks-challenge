0c467f0922951937532768b2da39181ccbc6857d
MFAC <ninafroes@gmail.com>
2020-07-10 16:30:13 +0200

    feat: Rename computerQuestions file to botMessageProvider and add more message options

---
+++ botMessageProvider.js
@@ -0,0 +1,18 @@
+export const computerQuestions = [
+  "Hello there, I'm Blorg. What is your name?",
+  "That's a pretty name. It's really nice to meet you.",
+  "How old are you?",
+  "Oh, you're so young! What are your hobbies?",
+  "Damn, you know how to have fun! Where do you live?",
+  "Wow, I've always wanted to live theeere! So cool. And what do you do for a living?",
+  "Awesome. If I weren't a robot, I'd do the same thing."
+]
+
+export const inviteForChat = [
+  "Whyyyy? I'm so coool! Let's chat!",
+  "Are you sure? I'm the coolest robot on Earth!",
+  "Ok, last chance. Do you wanna chat?",
+  "But why? Come on! Let's chat!",
+  "Just a quick chat!",
+  "Oh, you're making me sad. :( Let's chat!"
+];
--- computerQuestions.js
+++
@@ -1,6 +0,0 @@
-export const computerQuestions = [
-  "Hello there, I'm Blorg. What is your name?",
-  "It's really nice to meet you",
-  "How old are you?",
-  "Where do you live?"
-]
